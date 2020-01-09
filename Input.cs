233b6bd8d8ed1e8b1e4b4fa606d5ea38b86b2a86
Sunny <sunnydeng90@gmail.com>
2020-01-09 10:46:28 -0500

    Initial

---
+++ Input.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Texts_Dictionary
+{
+    public class Input
+    {
+        public static string resourcePath = @"C:\Users\choco\Documents\Project\Rider\Texts Dictionary\resources";
+
+        public static List<string> GetWords(string filename, string extension)
+        {
+            List<string> result = new List<string>();
+            try
+            {
+                string source = File.ReadAllText(resourcePath + "\\" + filename + "." + extension);
+                string[] results = Regex.Split(source, "( .,!?\\/:;)+");
+                results.ToList().ForEach(s =>
+                {
+                    if (!Regex.IsMatch(s, @"\d") && s.Length != 1)
+                    {
+                        result.Add(s);
+                    }
+                });
+            }
+            catch (IOException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.StackTrace);
+            }
+
+            if (result.Count > 0)
+            {
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Texts_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Input.resourcePath + "\\Dictionary.txt";
+            var listOfWords = Input.GetWords("NewFile1", "txt");
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            var newFile = File.AppendText(path);
+            newFile.Write(listOfWords);
+        }
+    }
+}
